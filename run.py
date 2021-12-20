ac1c45ab72946ff34abce2294c1a1852698d326b
devel1 <develop1@fake.mail>
2021-12-20 14:38:43 +0700

    add argv

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-from weather_app import app
+from weather_app import app, config
+import sys
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if len(sys.argv) > 1:
+    config.MYSQL_HOST = sys.argv[4]
+    config.MYSQL_DB = sys.argv[3]
+    config.MYSQL_USER = sys.argv[2]
+    config.MYSQL_PASSWORD = sys.argv[1]
