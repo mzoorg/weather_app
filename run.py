5eacc084effc5a3bc03c4add4bb00bceaa871f29
devel1 <develop1@fake.mail>
2021-12-20 21:01:22 +0700

    fix1

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from weather_app import app, config
-import sys
+from weather_app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if len(sys.argv) > 1:
-    config.MYSQL_HOST = sys.argv[4]
-    config.MYSQL_DB = sys.argv[3]
-    config.MYSQL_USER = sys.argv[2]
-    config.MYSQL_PASSWORD = sys.argv[1]
+    app.run(debug=True, host='0.0.0.0')
--- weather_app/config.py
+++ weather_app/config.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import date, timedelta
 
 CLIENT_ID = 'fcaf5431-dd36-49e0-9259-6fa5bd697d12'
@@ -8,7 +9,13 @@ SOURCES = {
     'id': 'SN18700'
 }
 
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'dbuser'
-MYSQL_PASSWORD = 'userpass'
-MYSQL_DB = 'dbname'
+if len(sys.argv) > 1:
+    MYSQL_HOST = sys.argv[4]
+    MYSQL_DB = sys.argv[3]
+    MYSQL_USER = sys.argv[2]
+    MYSQL_PASSWORD = sys.argv[1]
+else:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'dbuser'
+    MYSQL_PASSWORD = 'userpass'
+    MYSQL_DB = 'dbname'
