3cac7b0eddb94ffb1bc7bea4de6bc3a37f0617e0
devel1 <develop1@fake.mail>
2022-01-04 19:49:22 +0700

    fix11

--- app/weather_app/__init__.py
+++ app/weather_app/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asd90787d8fg7d7fg6789yghuih5ghjgfjdsf'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{config.MYSQL_USER}:{config.MYSQL_PASSWORD}@{config.MYSQL_HOST}/{config.MYSQL_DB}'
 db = SQLAlchemy(app)
-PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, path=None)
+metrics.start_http_server(config.EXPORTER_PORT)
 
 from weather_app import routes
